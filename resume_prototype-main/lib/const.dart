5fa9ae47a9a7942783c0570cb0b2765c72d5d731
rynzoken <rynzoken125@gmail.com>
2022-09-20 13:06:50 +0800

    first commit

---
+++ resume_prototype-main/lib/const.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'Personal Information';
+const cProfileFeature =
+    "Name: Jhyniecel P. Maynigo\n Age: 21\n Gender: Female\n Address: Songkoy, Calasiao, Pangasinan\n Contact No: 09519882173\n Date of Birth: September 1,2001\n" +
+        "Citizenship: Filipino\n Civil Status: Single";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color(0xFF3E2723);
+const cProfileBegColor = Color(0xFF6D4C41);
+const cProfileEndColor = Color(0xFFA1887F);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    'Bachelor of Information Technology\n Philippine College of Science and Technology-March 2019-2023\n' +
+        'Certificates:\n' +
+        '- NCII Computer System Servicing\n' +
+        '- Game Dev\n' +
+        '- Road to Success';
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xFF3E2723);
+const cEduBegColor = Color(0xFF6D4C41);
+const cEduEndColor = Color(0xFFA1887F);
+
+// Detail for experience item
+const cExperienceTopic = ' Personal Skills ';
+const cExperienceFeature =
+    '- Excellent written and verbal communication skills \n' +
+        '- Highly organized and efficient\n' +
+        '- Ability to work independently \n' +
+        '- Service-focused\n' +
+        '- Proven leadership skills and ability to work';
+const cExperienceLabel = 'Skills';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xFF3E2723);
+const cExpBegColor = Color(0xFF6D4C41);
+const cExpEndColor = Color(0xFFA1887F);
+
+final cCaptionTextSize = 20.0.sp;
+const cStartingIndex = 0;
